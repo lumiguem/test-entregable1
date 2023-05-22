85afdb561cc36fe4d68ccb6605beeaac02bfc7bf
Miguel Moreno <miguel.morenor@icloud.com>
2023-05-22 17:35:23 -0500

    app finished

---
+++ src/components/Quotebox.jsx
@@ -0,0 +1,13 @@
+import "./styles/Quotebox.css"
+const Quotebox = ({ phrase, handleChangeQuote }) => {
+    return (
+        <section className="quoteBox">
+            <article className="quoteBox__phrase">
+                <p>{phrase}</p>
+            </article>
+            <button className="quoteBox__btn" onClick={handleChangeQuote}>Update</button>
+        </section>
+    )
+}
+
+export default Quotebox
